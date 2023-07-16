58940340e2fabfac53cd2e0f5270a5e07c63b397
igorkorobko <nekto.korobko@gmail.com>
2023-07-16 20:58:29 +0300

    add imageinline to PlaceAdmin in admin panel.

--- place/admin.py
+++ place/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Images, Place
 
 
-@admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description_short', 'description_short', 'longitude', 'latitude']
+def format_preview_image(image, height='200px'):
+    return format_html(
+        '<img src="{url}" height="{height}"/>',
+        url=image.image.url,
+        height=height,
+    )
 
 
 @admin.register(Images)
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['place', 'position', 'image']
+
+
+class ImageInline(admin.TabularInline):
+    model = Images
+    extra = 0
+    readonly_fields = [format_preview_image]
+    list_display = ('images', format_preview_image, 'position',)
+    ordering = ['position']
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+    list_display = ['title', 'description_short', 'description_short', 'longitude', 'latitude']
