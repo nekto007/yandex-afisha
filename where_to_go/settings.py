f09c7370a38b28e90589a4e42167a7129e8c28af
igorkorobko <nekto.korobko@gmail.com>
2023-07-16 21:08:37 +0300

    add WYSIWYG-editor

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'place.apps.PlaceConfig',
     'adminsortable2',
+    'tinymce',
+
 ]
 
 MIDDLEWARE = [
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.start_page),
     path('places/<int:place_id>/', views.place_detail, name='place_detail'),
+    path('tinymce/', include('tinymce.urls')),
     path('', RedirectView.as_view(url='/place/', permanent=True)),
 ]
 
